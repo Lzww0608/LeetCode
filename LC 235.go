2e601ee9c6d8002f70d957739f7412d1379af8f6
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-02-25 23:41:12 +0800

    Add files via upload

---
+++ LC 235.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    for {
+        if root.Val > p.Val && root.Val > q.Val {
+            root = root.Left
+        } else if root.Val < p.Val && root.Val < q.Val {
+            root = root.Right
+        } else {
+            return root
+        }
+    }
+    return root
+}
