c56da804014144ed620a66e436e30fb338a64656
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-02-26 16:57:17 +0800

    Update LC 116.go

--- LC 116.go
+++ LC 116.go
@@ -12,23 +12,16 @@ func connect(root *Node) *Node {
 	if root == nil {
         return root
     }
-    q := []*Node{root}
-    for len(q) > 0 {
-        var preNode *Node 
-        for k := len(q); k > 0; k-- {
-            node := q[0]
-            q = q[1:]
-            if preNode != nil {
-                preNode.Next = node
-            }
-            preNode = node
-            if node.Left != nil {
-                q = append(q, node.Left)
-            }
-            if node.Right != nil {
-                q = append(q, node.Right)
-            }
+    var dfs func(*Node, *Node)
+    dfs = func(l *Node, r *Node) {
+        if l == nil || r == nil {
+            return 
         }
+        l.Next = r 
+        dfs(l.Left, l.Right)
+        dfs(r.Left, r.Right)
+        dfs(l.Right, r.Left)
     }
+    dfs(root.Left, root.Right)
     return root
 }
