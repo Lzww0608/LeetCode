a60c3bbe76428ba0f1755b837d27b286e88981fb
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-03-29 00:04:03 +0800

    Add files via upload

---
+++ LC 401.go
@@ -0,0 +1,14 @@
+func readBinaryWatch(turnedOn int) (ans []string) {
+    if turnedOn > 8 {
+        return 
+    }
+    for h := uint8(0); h < 12; h++ {
+        for m := uint8(0); m < 60; m++ {
+            if bits.OnesCount8(h)+bits.OnesCount8(m) == turnedOn {
+                ans = append(ans, fmt.Sprintf("%d:%02d", h, m))
+            }
+        }
+    }
+
+    return
+}
