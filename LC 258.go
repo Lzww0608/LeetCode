6869c1dfeb0ac9d1969b45a02938311ec3a66887
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-03-28 22:34:36 +0800

    Add files via upload

---
+++ LC 258.go
@@ -0,0 +1,12 @@
+func addDigits(num int) int {
+    for num >= 10 {
+        t := 0
+        for num > 0 {
+            t += num % 10
+            num /= 10
+        }
+        num = t
+    }
+
+    return num
+}
