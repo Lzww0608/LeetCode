28a5db4ab5aacf94f93b289c7c267434902e2993
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-03-28 20:56:08 +0800

    Add files via upload

---
+++ LC 234.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+    arr := []int{}
+    for head != nil {
+        arr = append(arr, head.Val)
+        head = head.Next
+    }
+
+    for i, j := 0, len(arr) - 1; i < j; i, j = i + 1, j - 1 {
+        if arr[i] != arr[j] {
+            return false
+        }
+    }
+    return true
+}
