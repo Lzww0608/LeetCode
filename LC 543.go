5d2bf09c25ddd86878774d90a24f8e2c130c1258
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-03-30 09:37:01 +0800

    Add files via upload

---
+++ LC 543.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func diameterOfBinaryTree(root *TreeNode) (ans int) {
+    var dfs func(*TreeNode) int 
+    dfs = func(root *TreeNode) int {
+        if root == nil {
+            return 0
+        }
+        l, r := dfs(root.Left), dfs(root.Right)
+        ans = max(ans, l + r)
+        return max(l, r) + 1
+    }
+    dfs(root)
+
+    return
+}
