31e429381447d0cdf89a75dc437541aeb05490cc
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-03-31 19:36:34 +0800

    Update LC 1462.go

--- LC 1462.go
+++ LC 1462.go
@@ -28,3 +28,50 @@ func checkIfPrerequisite(numCourses int, prerequisites [][]int, queries [][]int)
 
     return ans
 }
+
+
+func checkIfPrerequisite(numCourses int, prerequisites [][]int, queries [][]int) []bool {
+    g := make([][]bool, numCourses)
+    degree := make([]int, numCourses)
+    f := make([][]int, numCourses)
+    for i := range g {
+        g[i] = make([]bool, numCourses)
+    }
+    for _, e := range prerequisites {
+        a, b := e[0], e[1]
+        g[a][b] = true
+        degree[b]++
+        f[a] = append(f[a], b)
+    }
+
+    q := []int{}
+    for i, x := range degree {
+        if x == 0 {
+            q = append(q, i)
+        }
+    }
+
+    for len(q) > 0 {
+        i := q[0]
+        q = q[1:]
+        for _, j := range f[i] {
+            g[i][j] = true
+            for k := 0; k < numCourses; k++ {
+                g[k][j] = g[k][i] || g[k][j]
+            }
+            degree[j]--
+            if degree[j] == 0 {
+                q = append(q, j)
+            }
+        }
+        
+    }
+
+    ans := make([]bool, len(queries))
+    for i, q := range queries {
+        a, b := q[0], q[1]
+        ans[i] = g[a][b]
+    }
+
+    return ans
+}
