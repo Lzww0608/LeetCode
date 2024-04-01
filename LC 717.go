8a16856d40f052b8a9654471ee098ba226afaab2
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-04-01 12:05:07 +0800

    Add files via upload

---
+++ LC 717.go
@@ -0,0 +1,10 @@
+func isOneBitCharacter(bits []int) bool {
+    for i := 0; i < len(bits); i++ {
+        if i == len(bits) - 1 && bits[i] == 0 {
+            return true
+        } else if bits[i] == 1 {
+            i++
+        }
+    }
+    return false
+}
