0fa84b662a63f711e3ec93345af361b8483f638a
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-04-07 20:37:58 +0800

    Add files via upload

---
+++ LC 941.go
@@ -0,0 +1,15 @@
+func validMountainArray(arr []int) bool {
+    n, i := len(arr), 0
+    for i < n - 1 && arr[i] < arr[i+1] {
+        i++
+    }
+    if i == 0 || i == n - 1 {
+        return false
+    }
+
+    for i < n - 1 && arr[i] > arr[i+1] {
+        i++
+    }
+
+    return i == n - 1
+}
