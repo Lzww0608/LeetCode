aee37ccffff18dec1b3352212aa97fe84275e4a2
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-04-11 16:03:32 +0800

    Add files via upload

---
+++ LC 1047.go
@@ -0,0 +1,12 @@
+func removeDuplicates(s string) string {
+    st := []byte{}
+    for i := range s {
+        if len(st) > 0 && st[len(st)-1] == s[i] {
+            st = st[:len(st)-1]
+        } else {
+            st = append(st, s[i])
+        }
+    }
+
+    return string(st)
+}
