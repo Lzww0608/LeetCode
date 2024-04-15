e7095f5cc545ebefc6757cb9bd63ca5a34924e23
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-04-15 15:41:08 +0800

    Add files via upload

---
+++ LC 538.go
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func convertBST(root *TreeNode) *TreeNode {
+    sum := 0
+…    dfs(root)
+
+    return root
+}
