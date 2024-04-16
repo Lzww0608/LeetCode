067840fdfaa989c97b6cae9677b4005a6f346adc
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-04-16 17:36:05 +0800

    Add files via upload

---
+++ LC 1339.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+const MOD = 1e9 + 7
+…        return root.Val + l + r
+    }
+
+    dfs(root)
+    dfs2(root)
+
+    return int(ans % MOD)
+}
