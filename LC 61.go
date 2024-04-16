68509d92c6f9a9b199c7e13096748388b00676d7
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-04-16 12:50:45 +0800

    Add files via upload

---
+++ LC 61.go
@@ -0,0 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func rotateRight(head *ListNode, k int) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+…    tail.Next = head
+    return q
+}
