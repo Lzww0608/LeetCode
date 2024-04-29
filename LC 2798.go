2c0584240b8d69fc297ba578155d2500de2e8175
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-04-30 00:03:43 +0800

    Add files via upload

---
+++ LC 2798.go
@@ -0,0 +1,9 @@
+func numberOfEmployeesWhoMetTarget(hours []int, target int) (ans int) {
+    for _, x := range hours {
+        if x >= target {
+            ans++
+        }
+    }
+
+    return 
+}
