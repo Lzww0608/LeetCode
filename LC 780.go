d163eda543b6f66513627876a3da6594e3533ef3
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-04-29 23:07:52 +0800

    Add files via upload

---
+++ LC 780.go
@@ -0,0 +1,24 @@
+func reachingPoints(sx int, sy int, tx int, ty int) bool {
+    for tx >= sx && ty >= sy {
+        if tx == sx && ty == sy {
+            return true
+        }
+
+        if tx > ty {
+            if ty > sy {
+                tx %= ty
+            } else {
+                return (tx - sx) % ty == 0
+            }
+        } else {
+            if tx > sx {
+                ty %= tx
+            } else {
+                return (ty - sy) % tx == 0
+            }
+        }
+    }
+
+
+    return false
+}
