08fde33e699a221413bb9eb5ed9a6a542570d9ae
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-05-05 21:08:48 +0800

    Add files via upload

---
+++ LCR 070.go
@@ -0,0 +1,13 @@
+func singleNonDuplicate(nums []int) int {
+    l, r := 0, len(nums) - 1
+    for l < r  {
+        mid := l + ((r - l) >> 1)
+        if nums[mid] == nums[mid ^ 1] {
+            l = mid + 1
+        } else {
+            r = mid
+        }
+    }
+
+    return nums[l]
+}
