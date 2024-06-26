e5bc8748cef9210e5404b1ca7a17659c93b9189e
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-06-26 20:45:54 +0800

    Add files via upload

---
+++ LCCI/LCCI 03.02.go
@@ -0,0 +1,45 @@
+type MinStack struct {
+    st [][]int
+}
+
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+    return MinStack{[][]int{}}
+}
+
+
+func (this *MinStack) Push(x int)  {
+    if len(this.st) == 0 {
+        this.st = append(this.st, []int{x, x})
+    } else {
+        mn := this.st[len(this.st)-1][1]
+        this.st = append(this.st, []int{x, min(x, mn)})
+    }
+    return
+}
+
+
+func (this *MinStack) Pop()  {
+    this.st = this.st[:len(this.st)-1]
+}
+
+
+func (this *MinStack) Top() int {
+    return this.st[len(this.st)-1][0]
+}
+
+
+func (this *MinStack) GetMin() int {
+    return this.st[len(this.st)-1][1]
+}
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
