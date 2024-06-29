8ab4e2cc8f6867ea931fe51c8fb15283d0465481
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-06-29 22:44:43 +0800

    Add files via upload

---
+++ 1000/LC 675.go
@@ -0,0 +1,45 @@
+type neighbor struct {
+    to, weight int
+}
+
+func dijkstra(g [][]neighbor, start int) []int {
+    dist := make([]int, len(g))
+    for i := range dist {
+        dist[i] = math.MaxInt
+    }
+    dist[start] = 0
+    h := hp{{start, 0}}
+    for h.Len() > 0 {
+        t := heap.Pop(&h).(pair)
+        x := t.x
+        if dist[x] < t.dist {
+            continue
+        }
+        for _, e := range g[x] {
+            y := e.to
+            if d := dist[x] + e.weight; d < dist[y] {
+                dist[y] = d
+                heap.Push(&h, pair{y, d})
+            }
+        }
+    }
+
+    return dist
+}
+
+type pair struct {
+    x, dist int
+}
+type hp []pair
+func (h hp) Len() int {return len(h)}
+func (h hp) Less(i, j int) bool {return h[i].dist < h[j].dist}
+func (h hp) Swap(i, j int) {h[i], h[j] = h[j], h[i]}
+func (h *hp) Push(x any) {
+    *h = append(*h, x.(pair))
+}
+func (h *hp) Pop() (x any) {
+    old := *h
+    n := len(old)
+    x, *h = old[n-1], old[:n-1]
+    return
+}
