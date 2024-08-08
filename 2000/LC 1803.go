fc34c775d84baa3a26df70abf2c50a36555a4c3e
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-08-09 07:53:03 +0800

    Add files via upload

---
+++ 2000/LC 1803.go
@@ -0,0 +1,23 @@
+func countPairs(nums []int, low int, high int) (ans int) {
+    cnt := map[int]int{}
+    for _, x := range nums {
+        cnt[x]++
+    }
+
+    for high++; high > 0; high >>= 1 {
+        nxt := map[int]int{}
+        for x, c := range cnt {
+            if high & 1 > 0 {
+                ans += c * cnt[x ^ (high - 1)]
+            }
+            if low & 1 > 0 {
+                ans -= c * cnt[x ^ (low - 1)]
+            }
+            nxt[x >> 1] += c
+        }
+        cnt = nxt
+        low >>= 1
+    }
+
+    return ans >> 1
+}
