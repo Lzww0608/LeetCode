7ecde7d22f332b2a082cb1eecd260be14667f126
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-08-11 00:17:05 +0800

    Add files via upload

---
+++ 2000/LC 1035.go
@@ -0,0 +1,19 @@
+func maxUncrossedLines(a []int, b []int) int {
+    m, n := len(a), len(b)
+    f := make([][]int, m + 1)
+    for i := range f {
+        f[i] = make([]int, n + 1)
+    }
+    
+    for i := range a {
+        for j := range b {
+            if a[i] == b[j] {
+                f[i+1][j+1] = f[i][j] + 1
+            } else {
+                f[i+1][j+1] = max(f[i+1][j], f[i][j+1])
+            }
+        }
+    }
+
+    return f[m][n]
+}
