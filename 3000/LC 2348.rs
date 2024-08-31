471d8d0b4b5424971d161f74f23b0d3b360c47d1
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-08-31 11:14:51 +0800

    Add files via upload

---
+++ 3000/LC 2348.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn zero_filled_subarray(nums: Vec<i32>) -> i64 {
+        let mut ans: i64 = 0;
+        let mut cnt: i64 = 0;
+        for &x in &nums {
+            if x == 0 {
+                cnt += 1;
+                ans += cnt as i64;
+            } else {
+                cnt = 0;
+            }
+        }
+
+        ans
+    }
+}
