9c7621de1705e540000717c59ecbbfb11f56bb6f
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-12-22 09:25:31 +0800

    Add files via upload

---
+++ 2000/LC 1356.go
@@ -0,0 +1,13 @@
+
+import (
+	"math/bits"
+	"sort"
+)
+func sortByBits(arr []int) []int {
+    sort.Slice(arr, func(i, j int) bool {
+        a, b := bits.OnesCount(uint(arr[i])), bits.OnesCount(uint(arr[j]))
+        return a < b || a == b && arr[i] < arr[j]
+    })
+
+    return arr
+}
