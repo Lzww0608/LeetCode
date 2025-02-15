f4b4ed30a52f8cd2a2df43ecf3ad9165a17548a1
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-02-15 11:37:41 +0800

    Add files via upload

---
+++ 3000/LC 2518.go
@@ -0,0 +1,26 @@
+const MOD int = 1_000_000_007
+func countPartitions(nums []int, k int) int {
+    sum := 0
+    for _, x := range nums {
+        sum += x
+    }
+    if sum <= k {
+        return 0
+    }
+
+    f := make([]int, k)
+    ans := 1
+    f[0] = 1
+    for _, x := range nums {
+        ans = (ans * 2) % MOD
+        for i := k - 1; i >= x; i-- {
+            f[i] = (f[i] + f[i-x]) % MOD
+        }
+    }
+
+    for _, x := range f {
+        ans = (ans - x * 2 % MOD + MOD) % MOD
+    }
+
+    return ans
+}
