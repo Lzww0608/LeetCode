686e69130b7d7b91609cf88999be03f8f6c9aa64
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-03-10 09:30:18 +0800

    Add files via upload

---
+++ 3000/LC 2144.go
@@ -0,0 +1,11 @@
+func minimumCost(cost []int) (ans int) {
+    sort.Ints(cost)
+    for i := len(cost) - 1; i >= 0; i -= 3 {
+        ans += cost[i]
+        if i - 1 >= 0 {
+            ans += cost[i-1]
+        }
+    }
+
+    return
+}
