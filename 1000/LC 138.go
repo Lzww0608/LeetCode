b9156accab07af5f3dab8c1a419e4d97f71f09cd
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-05-08 20:13:45 +0800

    Add files via upload

---
+++ 1000/LC 138.go
@@ -0,0 +1,34 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ *     Random *Node
+ * }
+ */
+
+func copyRandomList(head *Node) *Node {
+    if head == nil {
+        return nil 
+    }
+
+    for cur := head; cur != nil; cur = cur.Next.Next {
+        cur.Next = &Node{Val: cur.Val, Next: cur.Next}
+    }
+
+    for cur := head; cur != nil; cur = cur.Next.Next {
+        if cur.Random != nil {
+            cur.Next.Random = cur.Random.Next
+        }
+    }
+
+    newHead := head.Next 
+    cur := head 
+    for ; cur.Next.Next != nil; cur = cur.Next {
+        clone := cur.Next 
+        cur.Next = clone.Next 
+        clone.Next = clone.Next.Next
+    }
+    cur.Next = nil 
+    return newHead
+}
