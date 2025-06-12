6bacabbc5a2120867c5b105b18240be3504615a7
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-06-12 09:04:31 +0800

    Add files via upload

---
+++ 4000/LC 3423.go
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn max_adjacent_distance(nums: Vec<i32>) -> i32 {
+        nums.iter()
+            .zip(nums.iter().cycle().skip(1))
+            .map(|(&a, &b)| (a - b).abs())
+            .max()
+            .unwrap()
+    }
+}
