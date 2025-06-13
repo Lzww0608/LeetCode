9b0a2e0dee5a30dfb980ea1295a9be171d50f79b
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-06-14 00:10:54 +0800

    Add files via upload

---
+++ 3000/LC 2566.go
@@ -0,0 +1,12 @@
+func minMaxDifference(num int) int {
+    mx := num 
+    s := strconv.Itoa(num)
+    for _, c := range s {
+        if c != '9' {
+            mx, _ = strconv.Atoi(strings.ReplaceAll(s, string(c), "9"))
+            break
+        }
+    }
+    mn, _ := strconv.Atoi(strings.ReplaceAll(s, s[:1], "0"))
+    return mx - mn
+}
