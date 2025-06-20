3017de3ffb4d7aab514162a48bb5231f1ebc1987
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-06-20 17:59:12 +0800

    Add files via upload

---
+++ 1000/LC 714.go
@@ -0,0 +1,11 @@
+func maxProfit(prices []int, fee int) int {
+    n := len(prices)
+    f := make([][2]int, n)
+    f[0][0], f[0][1] = -prices[0] - fee, 0
+    for i := 1; i < n; i++ {
+        f[i][0] = max(f[i-1][0], f[i-1][1] - prices[i] - fee)
+        f[i][1] = max(f[i-1][1], f[i-1][0] + prices[i])
+    }
+
+    return f[n-1][1]
+}
