c38a92f2e744328ca1e3a3e34c8f4c8cac3e1846
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-06-22 08:50:36 +0800

    Add files via upload

---
+++ 3000/LC 2138.go
@@ -0,0 +1,13 @@
+func divideString(s string, k int, fill byte) []string {
+    n := len(s)
+    ans := make([]string, 0, (n + k - 1) / k)
+    for i := 0; i < n; i += k {
+        if i + k <= n {
+            ans = append(ans, s[i:i+k])
+        } else {
+            ans = append(ans, s[i:] + strings.Repeat(string(fill), k - n + i))
+        }
+    }
+
+    return ans
+}
