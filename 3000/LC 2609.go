1a4780a5b7762593f76aa9a510e8511cf28cb62a
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-07-04 21:44:05 +0800

    Add files via upload

---
+++ 3000/LC 2609.go
@@ -0,0 +1,16 @@
+func findTheLongestBalancedSubstring(s string) (ans int) {
+    zero, one := 0, 0
+    for _, c := range s {
+        if c == '0' {
+            if one > 0 {
+                zero, one = 0, 0
+            }
+            zero++
+        } else {
+            one++
+            ans = max(ans, min(zero, one) * 2)
+        }
+    }
+
+    return
+}
