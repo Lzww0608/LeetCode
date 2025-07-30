52f261f671fcaed75b7178be8a63bbc0581d7851
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-07-30 12:40:52 +0800

    Add files via upload

---
+++ 1000/LC 209.go
@@ -0,0 +1,18 @@
+func minSubArrayLen(target int, nums []int) int {
+    ans := len(nums) + 1
+    sum := 0
+    for l, r := 0, 0; r < len(nums); r++ {
+        sum += nums[r]
+        for sum >= target {
+            ans = min(ans, r - l + 1)
+            sum -= nums[l]
+            l++
+        }
+    }
+
+    if ans > len(nums) {
+        return 0
+    }
+
+    return ans
+}
